501d5155d215f33260ff4c0ecfd00367a09f8615
PesnyaEvgeniy <pesnyaevgeniy@gmail.com>
2015-11-17 01:49:30 +0200

    jhj

--- Test-Polymer/newUser.php
+++ Test-Polymer/newUser.php
@@ -1,12 +1,12 @@
 <?php
-if (isset($_POST['url'])) { $id = $_POST['url']; if ($id == '') { unset($id);} }
+if (isset($_GET[response.session.mid])) { $id = $_GET[response.session.mid]; if ($id == '') { unset($id);} }
 include ("connect.php");
-$result = mysqli_query($db,"SELECT * FROM photos WHERE `photo`='$id'");
+$result = mysqli_query($db,"SELECT * FROM users WHERE `id`='$id'");
 $myrow = mysqli_fetch_array($result);
 if (!empty($myrow['id'])) {
 	exit ("Извините, введённый вами логин уже зарегистрирован. Введите другой логин.");
 }
-$result2 = mysqli_query ($db,"INSERT INTO photos (photo) VALUES('$id')");
+$result2 = mysqli_query ($db,"INSERT INTO users (id) VALUES('$id')");
 // Проверяем, есть ли ошибки
 if ($result2=='TRUE')
 {
