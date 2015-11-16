98bc3e4c1bde0b69fe7f04d6f0d5a8ab04225242
PesnyaEvgeniy <pesnyaevgeniy@gmail.com>
2015-11-16 20:31:03 +0200

    gg

--- Test-Polymer/connect.php
+++ Test-Polymer/connect.php
@@ -3,17 +3,8 @@ $dblocation = "mysql.hostinger.com.ua";
 $dbname = "photos";
 $dbuser = "u300663046_data1";
 $dbpasswd = "101013born";
-$dbcnx = @mysql_connect($dblocation,$dbuser,$dbpasswd);
-if (!$dbcnx) 
-{
-  echo( "<P>¬ насто€щий момент сервер базы данных не доступен, поэтому 
-            корректное отображение страницы невозможно.</P>" );
-  exit();
-}
-if (!@mysql_select_db($dbname, $dbcnx)) 
-{
-  echo( "<P>¬ насто€щий момент база данных не доступна, поэтому
-            корректное отображение страницы невозможно.</P>" );
-  exit();
-}
+mysql_connect($dblocation,$dbuser,$dbpasswd);
+
+mysql_select_db($dbname, $dbcnx)) 
+
 ?>
