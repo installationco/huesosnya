e3ec26676572cc1ef865962f6ae3c931d5a83485
PesnyaEvgeniy <pesnyaevgeniy@gmail.com>
2015-11-17 00:32:09 +0200

    sdgd

--- Test-Polymer/connect.php
+++ Test-Polymer/connect.php
@@ -1,4 +1,4 @@
 <?php
-mysqli_connect("mysql.hostinger.com.ua","u300663046_data1","101013born","u300663046_data1");
+$db = mysqli_connect("mysql.hostinger.com.ua","u300663046_data1","101013born","u300663046_data1");
 
 ?>
