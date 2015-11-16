96c30463ab5915e6a97201db9f294bf62e7ba6e1
PesnyaEvgeniy <pesnyaevgeniy@gmail.com>
2015-11-16 22:07:37 +0200

    kg

--- Test-Polymer/connect.php
+++ Test-Polymer/connect.php
@@ -3,7 +3,7 @@ $dblocation = "mysql.hostinger.com.ua";
 $dbname = "photos";
 $dbuser = "u300663046_data1";
 $dbpasswd = "101013born";
-mysql_connect($dblocation,$dbuser,$dbpasswd);
+$dbcnx = mysql_connect($dblocation,$dbuser,$dbpasswd);
 
 mysql_select_db($dbname, $dbcnx);
 
