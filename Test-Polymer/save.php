9eb0ec6917e335d9ef29db8d7e42ed90120d4118
PesnyaEvgeniy <pesnyaevgeniy@gmail.com>
2015-11-17 00:26:46 +0200

    yf

--- Test-Polymer/save.php
+++ Test-Polymer/save.php
@@ -1,12 +1,12 @@
 <?php
 if (isset($_POST['url'])) { $id = $_POST['url']; if ($id == '') { unset($id);} }
 include ("connect.php");
-$result = mysqli_query($db,"SELECT * FROM photos WHERE `photo`='$id'");
+$result = mysqli_query("SELECT * FROM photos WHERE `photo`='$id'");
 $myrow = mysqli_fetch_array($result);
 if (!empty($myrow['id'])) {
 	exit ("Извините, введённый вами логин уже зарегистрирован. Введите другой логин.");
 }
-$result2 = mysql_query ("INSERT INTO photos (photo) VALUES('$id')");
+$result2 = mysqli_query ("INSERT INTO photos (photo) VALUES('$id')");
 // Проверяем, есть ли ошибки
 if ($result2=='TRUE')
 {
